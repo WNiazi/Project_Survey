866503d2c1ca25387d712af1972bb9eb45c98c74
Wajma Niazi <w1niazi@hotmail.com>
2021-11-04 09:58:58 -0700

    It will aid in click function

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+const question1 = $("#question1").val();
+const question2=$("#question2").val(); 
+const question3=$("#question3").val();
+const age = $("input:radio[name=age]:checked").val(); 
+const question5=$("#question5").val(); 
