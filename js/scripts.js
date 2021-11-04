ab10a1d0df35625b2489dc3a7a82f63786326882
Wajma Niazi <w1niazi@hotmail.com>
2021-11-04 10:33:38 -0700

    It adds small amount of styling for box1 and heading

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,19 @@ const question2=$("#question2").val();
 const question3=$("#question3").val();
 const age = $("input:radio[name=age]:checked").val(); 
 const question5=$("#question5").val(); 
+
+const question1 =document.querySelector ("box1");
+const question2 =document.querySelector ("box2");
+const question3 =document.querySelector ("box3");
+const question4 =document.querySelector ("box4");
+const question5=document.querySelector ("box5");
+
+const container=document.querySelector("container"); 
+const box6 =document.querySelector ("box6"); 
+
+//first question 
+//first whole display none
+box1.addEventListener("click", function() {
+  question1.style.display="none";
+  question2.style.display="block";
+}); 
